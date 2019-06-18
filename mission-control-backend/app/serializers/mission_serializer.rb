c7b697ca5961efe33e8997ad58a9d56620eb5947
jkshareef <jamalkshareef@gmail.com>
2019-06-18 09:52:32 -0700

    completed mission serializer

--- mission-control-backend/app/serializers/mission_serializer.rb
+++ mission-control-backend/app/serializers/mission_serializer.rb
@@ -1,3 +1,26 @@
 class MissionSerializer < ActiveModel::Serializer
-  attributes :id
+  def initialize(mission_object)
+    @mission = mission_object
+  end
+
+  def to_serialized_json
+    options = {
+      include: {
+        crews: {
+          only: [:name, :skill, :rating, :cost, :gender]
+        },
+        destinations: {
+          only: [:name, :location, :distance, :object_source]
+        },
+        events: {
+          only: [:content, :resource_cost, :target_resource, :acceleration]
+        },
+        resources: {
+          only: [:food, :oxygen, :fuel, :medicine]
+        }
+      },
+      except: [:created_at, :updated_at],
+    }
+    @mission.to_json(options)
+  end
 end
