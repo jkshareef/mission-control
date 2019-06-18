d37a584300b86677ff6f65d84b430bf502173bf8
jkshareef <jamalkshareef@gmail.com>
2019-06-18 01:44:49 -0700

    added mission post

--- mission-control-backend/app/controllers/missions_controller.rb
+++ mission-control-backend/app/controllers/missions_controller.rb
@@ -1,5 +1,10 @@
 class MissionsController < ApplicationController
 
+
+    def index
+        missions = Mission.all
+        render json: missions
+    end
     def create
         mission = Mission.create
         render json: mission
