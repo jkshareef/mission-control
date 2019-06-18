31f4aff4c65ebdd9b143cdd6086a62d2a76a3f54
jkshareef <jamalkshareef@gmail.com>
2019-06-17 17:09:02 -0700

    added crew constants to index.js

--- mission-control-frontend/src/index.js
+++ mission-control-frontend/src/index.js
@@ -3,8 +3,46 @@ import './style.css';
 import Icon from './icon.svg';
 // import * as THREE from 'three';
 
+    const rhiannon = {name: "Rhiannon Dade", skill: "Biology", gender: "female", expertise: 99, cost: 48000}
+    const zhenwhei = {name: "Zhenwhei Yang", skill: "Physicist", gender: "female", expertise: 82, cost: 40000}
+    const kristof = {name: "Kristof Jenner", skill: "Mechanic", gender: "male", expertise: 79, cost: 32000}
+    const yuri = {name: "Yuri Kochalev", skill: "Navigator", gender: "male", expertise: 92, cost: 58000}
+    const bruce = {name: "Bruce Maximoff", skill: "Gunner", gender: "male", expertise: 86, cost: 30000}
+    const terry = {name: "Terry Achebe", skill: "Engineer", gender: "male", expertise: 87, cost: 65000}
+    const lucy = {name: "Lucy Berry", skill: "Chemist", gender: "female", expertise: 90, cost: 46000}
+    const prakash = {name: "Prakash Manvi", skill: "Mechanic", gender: "male", expertise: 79, cost: 32000}
+    const loretta = {name: "Loretta Campbell", skill: "Leader", gender: "female", expertise: 83, cost: 50000}
+    const abraham = {name: "Abraham Ducet", skill: "Driller", gender: "male", expertise: 79, cost: 32000}
+    const haley = {name: "Haley Norris", skill: "Medic", gender: "female", expertise: 88, cost: 60000}
+    const arlene = {name: "Arlene McKinney", skill: "Mechanic", gender: "female", expertise: 62, cost: 20000}
+    const ken = {name: "Ken Blevins", skill: "Mechanic", gender: "male", expertise: 73, cost: 32000}
 
-// var scene = new THREE.Scene();
+    const moon = {name: "Moon", object_source:"/images_3D/Moon_1_3474.glb"}
+    const mars = {name: "Mars", object_source: "/images_3D/Mars_1_6792.glb" }
+    const venus = {name: "Venus", object_source: "/images_3D/Venus_1_12103.glb"} 
+    const mercury = {name: "Mercury",object_source:"/images_3D/Mercury_1_4878.glb"}
+    const jupiter = {name: "Jupiter", object_source:"/images_3D/Jupiter_1_142984.glb"}
+    const saturn = {name: "Saturn",object_source:"/images_3D/Saturn_1_120536.glb"}
+    const uranus = {name: "Uranus",object_source:"/images_3D/Uranus_1_51118.glb"}
+    const neptune = {name: "Neptune",object_source:"/images_3D/Neptune_1_49528.glb"}
+    const pluto = {name: "Pluto",object_source: "/images_3D/Pluto_1_2374.glb"}
+    const titan = {name: "Titan",object_source: "/images_3D/TitanSurface_1_5150.glb"}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // var scene = new THREE.Scene();
 // var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 // var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 // var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -46,15 +84,19 @@ import Icon from './icon.svg';
 // var line = new THREE.Line( geometry, material );
 // scene.add( line );
 // renderer.render( scene, camera );
-
+// var scene = new THREE.Scene();
+// var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+// var renderer = new THREE.WebGLRenderer();
+// renderer.setSize( window.innerWidth, window.innerHeight );
+// document.body.appendChild( renderer.domElement );
 
 // var THREE = window.THREE = require('three')
 // require('three/examples/js/loaders/GLTFLoader')
 
-// var loader = new THREE.GLTFLoader();
+// var loader = new THREE.GLTFLoader().setPath( '/images_3D/');
 
 
-// loader.load( 'http://localhost:8000/Mars_1_6792.glb', function ( gltf ) {
+// loader.load( 'Mars_1_6792.glb', function ( gltf ) {
 
 // 	scene.add( gltf.scene );
 
@@ -63,5 +105,6 @@ import Icon from './icon.svg';
 // 	console.error( error );
 
 // } );
+
 
 
